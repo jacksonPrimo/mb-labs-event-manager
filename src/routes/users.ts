56ec0fb4ff415477e72bb0a6f34faa87e28bf80d
jacksonPrimo <jackson.primo12@gmail.com>
2021-11-10 10:03:47 -0300

    added authentication middleware in user routes

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import userController from 'src/controllers/user.controller';
+import { logged } from 'src/middlewares/authorization.middleware';
 const router = Router();
 router.get('/', userController.get);
-router.put('/', userController.update);
+router.put('/', logged, userController.update);
 
 export default router;
