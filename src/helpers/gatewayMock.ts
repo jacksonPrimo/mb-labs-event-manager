47135fbe7231d19016194dca0907c9fda07dbaf3
jacksonPrimo <jackson.primo12@gmail.com>
2021-11-10 23:12:17 -0300

    bug fix in getway mock

--- src/helpers/gatewayMock.ts
+++ src/helpers/gatewayMock.ts
@@ -11,8 +11,7 @@ export async function pay (payment: paymentObject) {
       id: payment.metadata.reserveId,
       status: (Math.random() * 10) < 5 ? 'paid' : 'fail'
     };
-    const host = process.env.HOST || 'http://localhost';
-    const port = process.env.PORT || 3000;
-    await axios.put(`${host}:${port}/webhooks/reserves`, objectToSend);
+    const host = process.env.HOST || 'http://localhost:3000';
+    await axios.put(`${host}/webhooks/reserves`, objectToSend);
   }, 5000);
 }
